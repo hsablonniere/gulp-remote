0bd0664b6d95f10a3f7bd2569e6d4207e339a876
phi <phi1618jp@gmail.com>
2017-04-17 16:16:38 +0900

    enable local file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var URL = require('url');
 var path = require('path');
+var fs = require('fs');
 var request = require('request');
 var es = require('event-stream');
 var through = require('through2');
@@ -7,18 +9,37 @@ var File = require('vinyl');
 module.exports = function(urls, options) {
   return es.readArray(urls).pipe(es.map(function(data, cb) {
     var url = data;
-    request({
-      url: url,
-    }, function(error, response, body) {
-      if (!error) {
-        var file = new File({
-          cwd: '/',
-          base: path.dirname(url),
-          path: url,
-          contents: new Buffer(body),
-        });
-        cb(null, file);
-      }
-    });
+    var urlInfo = URL.parse(url);
+
+    if (urlInfo.protocol) {
+      request({
+        url: url,
+      }, function(error, response, body) {
+        if (!error && (response.statusCode >= 200 && response.statusCode < 300)) {
+          var file = new File({
+            cwd: '/',
+            base: path.dirname(url),
+            path: url,
+            contents: new Buffer(body),
+          });
+          cb(null, file);
+        }
+        else {
+          error = new Error("Request " + url + " failed with status code:" + response.statusCode);
+          cb(error);
+        }
+      });
+    }
+    else {
+      var file = new File({
+        cwd: '/',
+        base: path.dirname(url),
+        path: url,
+        contents: fs.readFileSync(url),
+        // contents: request(requestOptions).pipe(through2())
+      });
+
+      cb(null, file);
+    }
   }));
 };
--- test/gulpfile.js
+++ test/gulpfile.js
@@ -8,6 +8,7 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   return gulp.remote([
+      './a.js',
       'https://cdnjs.cloudflare.com/ajax/libs/riot/3.3.2/riot.min.js',
       'https://cdn.jsdelivr.net/riot-route/3.0.0/route.min.js',
     ])
